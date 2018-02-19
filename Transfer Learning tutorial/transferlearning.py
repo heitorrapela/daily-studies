1018987ba3759e28a8150b3f790f8816db0f8b44
heitorrapela <hrm@cin.ufpe.br>
2018-02-19 10:20:22 -0300

    Transfer Learning tutorial: Add visualization

---
+++ Transfer Learning tutorial/transferlearning.py
@@ -0,0 +1,75 @@
+# License: BSD
+# Author: Sasank Chilamkurthy
+# Heitor R.: I rewrite all tutorial to learn and fix the concepts
+
+# The problem solved by the tutorial its a 
+# ants and bees classifier
+from __future__ import print_function, division
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+from torch.autograd import Variable
+import numpy as np 
+import torchvision
+from torchvision import datasets, models, transforms
+import matplotlib.pyplot as plt 
+import time
+import os
+import copy
+
+# interactive mode
+plt.ion() 
+
+## Load Data
+
+# Data augmentation and normalization for training
+# Just normalization for validation
+data_transforms = {
+	'train': transforms.Compose([
+		transforms.RandomSizedCrop(224),
+		transforms.RandomHorizontalFlip(),
+		transforms.ToTensor(),
+		transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+		]),
+	'val': transforms.Compose([
+		transforms.Scale(256),
+		transforms.CenterCrop(224),
+		transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])		]),
+}
+
+data_dir = 'hymenoptera_data'
+image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x), data_transforms[x])
+	for x in ['train', 'val']
+}
+dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=4, shuffle=True, num_workers=4)
+	for x in ['train', 'val']
+}
+
+dataset_size = {x: len(image_datasets[x]) for x in ['train', 'val']}
+class_names = image_datasets['train'].classes
+
+use_gpu = torch.cuda.is_available()
+
+## Visualize Data
+
+def imshow(inp, title=None):
+	inp = inp.numpy().transpose((1,2,0))
+	mean = np.array([0.485, 0.456, 0.406])
+	std = np.array([0.229, 0.224, 0.225])
+	inp = std * inp + mean
+	inp = np.clip(inp,0,1)
+	plt.imshow(inp)
+	if title is not None:
+		plt.title(title)
+	plt.pause(1.000)  # pause a bit so that plots are updated
+
+# Get a batch of training data
+inputs, classes = next(iter(dataloaders['train']))
+
+# Make a grid from batch
+out = torchvision.utils.make_grid(inputs)
+
+imshow(out, title=[class_names[x] for x in classes])
