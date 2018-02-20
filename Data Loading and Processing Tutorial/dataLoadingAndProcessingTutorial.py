8ef9a85f4d4572c1d0122294ee06e7179d1157e2
heitorrapela <hrm@cin.ufpe.br>
2018-02-20 09:38:21 -0300

    Open data from landmarks faces dlib

---
+++ Data Loading and Processing Tutorial/dataLoadingAndProcessingTutorial.py
@@ -0,0 +1,25 @@
+from __future__ import print_function, division
+import os
+import torch
+import pandas as pd
+from skimage import io, transform
+import numpy as numpy
+import matplotlib.pyplot as plt 
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils
+
+# ignore warnings
+import warnings
+warnings.filterwarnings("ignore")
+plt.ion()
+
+landmarks_frame = pd.read_csv('./faces/face_landmarks.csv')
+
+n = 65
+img_name = landmarks_frame.iloc[n,0]
+landmarks = landmarks_frame.iloc[n, 1:].as_matrix()
+landmarks = landmarks.astype('float').reshape(-1,2)
+
+print('Image name: {}'.format(img_name))
+print('Landmarks shape: {}'.format(landmarks.shape))
+print('First 4 Landmarks: {}'.format(landmarks[:4]))
